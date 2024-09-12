5a51b4ebf01444ee336e35f077b4ffe7a0f7b037
Esraa Khalifa <esraakhalifa208@yahoo.com>
2024-09-12 03:45:45 +0300

    Update Response

--- lib/paymob_iframe.dart
+++ lib/paymob_iframe.dart
@@ -1,6 +1,6 @@
 part of 'paymob_payment.dart';
 
-class PaymobIFrame extends StatelessWidget {
+class PaymobIFrame extends StatefulWidget {
   const PaymobIFrame({
     Key? key,
     required this.redirectURL,
@@ -14,36 +14,77 @@ class PaymobIFrame extends StatelessWidget {
     required BuildContext context,
     required String redirectURL,
     void Function(PaymobResponse)? onPayment,
-  }) async {
-    final completer = Completer<PaymobResponse?>();
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) {
-          return PaymobIFrame(
-            redirectURL: redirectURL,
-            onPayment: (response) {
-              completer.complete(response);
-              Navigator.pop(context);
-            },
-          );
-        },
-      ),
-    );
-    return completer.future;
-  }
+  }) =>
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) {
+            return PaymobIFrame(
+              onPayment: onPayment,
+              redirectURL: redirectURL,
+            );
+          },
+        ),
+      );
 
   @override
-  Widget build(BuildContext context) {
-    _launchURL(redirectURL);
-    return Container();
+  State<PaymobIFrame> createState() => _PaymobIFrameState();
+}
+
+class _PaymobIFrameState extends State<PaymobIFrame> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _launchURL(widget.redirectURL);
   }
 
   Future<void> _launchURL(String url) async {
     final Uri uri = Uri.parse(url);
     if (await canLaunchUrl(uri)) {
+      setState(() {
+        _isLoading = false;
+      });
       await launchUrl(uri);
+      _handleUrlResponse(url);
     } else {
+      setState(() {
+        _isLoading = false;
+      });
       throw 'Could not launch $url';
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _isLoading
+            ? const CircularProgressIndicator.adaptive()
+            : Text('Redirecting...'),
+      ),
+    );
+  }
+
+  void _handleUrlResponse(String url) {
+    if (url.contains('txn_response_code') &&
+        url.contains('success') &&
+        url.contains('id')) {
+      final params = _getParamFromURL(url);
+      final response = PaymobResponse.fromJson(params);
+      if (widget.onPayment != null) {
+        widget.onPayment!(response);
+      }
+      Navigator.pop(context, response);
+    }
+  }
+
+  Map<String, dynamic> _getParamFromURL(String url) {
+    final uri = Uri.parse(url);
+    Map<String, dynamic> data = {};
+    uri.queryParameters.forEach((key, value) {
+      data[key] = value;
+    });
+    return data;
   }
 }
